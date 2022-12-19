ae569ba2f01ccb7a53f00488ba41bd6717673c2e
miguelalmeida2 <miguelalmeida991@protonmail.com>
2022-12-19 13:51:03 +0000

    minor changes to ex5 answer and 7 .env file

--- SegInfo/src/S2/Ex7/server.js
+++ SegInfo/src/S2/Ex7/server.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 var jwt = require('jsonwebtoken')
 const res = require('express/lib/response')
 const { json } = require('body-parser')
-const { execute, enforce} = require('./casbinHelper')
+const { execute, enforce } = require('./casbinHelper')
 let state_DB = []
 
 const methodToAction = {
@@ -27,13 +27,13 @@ const CALLBACK = 'callback'
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
- 
+
 const login = (req, resp) => {
     let state = crypto.randomUUID()
-    state_DB.push({state})
+    state_DB.push({ state })
     resp.redirect(302,
         'https://accounts.google.com/o/oauth2/v2/auth?'
-        + 'client_id='+ CLIENT_ID +'&'
+        + 'client_id=' + CLIENT_ID + '&'
         + 'scope=openid%20email%20https://www.googleapis.com/auth/tasks&'
         + `state=${state}&`
         + 'response_type=code&'
@@ -42,60 +42,61 @@ const login = (req, resp) => {
 }
 
 const callback = (req, resp) => {
-    if(verifyState(req.query.state)){
+    if (verifyState(req.query.state)) {
         request
             .post(
-                { 
+                {
                     url: 'https://www.googleapis.com/oauth2/v3/token',
                     // body parameters
                     form: {
                         code: req.query.code,
                         client_id: CLIENT_ID,
                         client_secret: CLIENT_SECRET,
-                        redirect_uri: 'http://localhost:3001/'+CALLBACK,
+                        redirect_uri: 'http://localhost:3001/' + CALLBACK,
                         grant_type: 'authorization_code'
                     }
-                }, 
-                function(err, httpResponse, body){
-                    if(err){
+                },
+                function (err, httpResponse, body) {
+                    if (err) {
                         resp.redirect('/error')
                     }
                     var json_response = JSON.parse(body);
                     var jwt_payload = jwt.decode(json_response.id_token)
-                    
-                    //resp.cookie("AuthCookie",json_response.access_token,{maxAge:3600,httpOnly:true})
+
+                    //resp.cookie("AuthCookie", json_response.access_token, { maxAge: 3600, httpOnly: true })
                     const access_token = json_response.access_token
                     request
                         .get(
                             {
                                 url: 'https://tasks.googleapis.com/tasks/v1/users/@me/lists',
-                                headers:{
+                                headers: {
                                     Authorization: `Bearer ${json_response.access_token}`
                                 }
                             },
-                            function(err, httpResponse, body) { 
-                                if(err){
+                            function (err, httpResponse, body) {
+                                if (err) {
                                     resp.redirect('/error')
                                 }
 
                                 var json_response = JSON.parse(body);
-                                
+
                                 state_DB = state_DB.map(index => {
-                                    if(index.state == req.query.state) {
+                                    if (index.state == req.query.state) {
                                         index.token = access_token
                                         index.email = jwt_payload.email
-                                        resp.cookie("AuthCookie",index.state)
+                                        resp.cookie("AuthCookie", index.state)
                                     }
                                     return index
                                 })
-                                //resp.cookie("AuthCookie",access_token)
+                                //resp.cookie("AuthCookie", access_token)
+                                //console.log(json_response)
                                 let listHtml = json_response.items.map(item => `<div><a href = '/list/${item.id}'>${item.title}</a></div>`).join("<br></br>")
                                 resp.send(listHtml)
                             }
                         )
                 }
             );
-    }else{
+    } else {
         resp.redirect('/error')
     }
 }
@@ -106,43 +107,43 @@ const getTaskList = (req, resp) => {
         .get(
             {
                 url: `https://tasks.googleapis.com/tasks/v1/lists/${req.params.id}/tasks`,
-                headers:{
+                headers: {
                     Authorization: `Bearer ${token}`
                 }
             },
-            function(err, httpResponse, body){
-                if(err){
+            function (err, httpResponse, body) {
+                if (err) {
                     resp.redirect('/error')
                 }
 
                 var json_response = JSON.parse(body);
                 let listHtml = json_response.items.map(item => `<div><h1 href = '/task/${item.id}'>${item.title}</h1><p>${item.notes ? item.notes : "There's no notes for this task"}</p></div>`).join("<br></br>")
-                listHtml += "<br></br><h1>Create a new task</h1><form method= 'post'>" + 
-                "<label for='title'>Title:</label><br>" +
-                "<input type='text' id='title' name='title' value=''><br>" +
-                "<label for='notes'>Notes:</label><br>" +
-                "<input type='text' id='notes' name='notes' value=''><br><br>" +
-                "<input type='submit' value='Submit'>" +
-                "</form>"
+                listHtml += "<br></br><h1>Create a new task</h1><form method= 'post'>" +
+                    "<label for='title'>Title:</label><br>" +
+                    "<input type='text' id='title' name='title' value=''><br>" +
+                    "<label for='notes'>Notes:</label><br>" +
+                    "<input type='text' id='notes' name='notes' value=''><br><br>" +
+                    "<input type='submit' value='Submit'>" +
+                    "</form>"
                 resp.send(listHtml)
             }
         )
 }
 
-const postTask = (req,resp) => {
+const postTask = (req, resp) => {
     const token = getToken(req.cookies.AuthCookie)
     request
         .post(
             {
                 url: `https://tasks.googleapis.com/tasks/v1/lists/${req.params.id}/tasks`,
-                headers:{
+                headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
                 },
-                body:JSON.stringify({ title: req.body.title, notes: req.body.notes })
+                body: JSON.stringify({ title: req.body.title, notes: req.body.notes })
             },
-            function(err, httpResponse, body){
-                if(err){
+            function (err, httpResponse, body) {
+                if (err) {
                     resp.redirect('/error')
                 }
                 resp.redirect(`/list/${req.params.id}`)
@@ -155,7 +156,7 @@ const error = (req, resp) => {
 }
 
 const unauthorized = (req, resp) => {
-    resp.send(`<p>You are not allowed to access this feature.</p> <img src="https://st.depositphotos.com/3332767/4585/i/950/depositphotos_45854107-stock-photo-man-holding-stop-sign.jpg" width="1000" height="600">`)
+    resp.send(`<p>You are not allowed to access this feature.</p>`)
 }
 
 const authMiddleware = (req, resp, next) => {
@@ -172,7 +173,7 @@ const rbacMiddleware = (req, resp, next) => {
     const act = methodToAction[req.method];
     enforce(sub, obj, act).then(decision => {
         execute(decision)
-        if(decision.res) next()
+        if (decision.res) next()
         else resp.redirect('/unauthorized')
     })
 }
